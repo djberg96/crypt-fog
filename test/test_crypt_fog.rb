f4f94269e08d6f41990e7244f88a321e9820c1d2
Daniel Berger <djberg96@gmail.com>
2021-04-06 13:23:35 -0400

    Updated source, version constant now frozen.

--- test/test_crypt_fog.rb
+++ test/test_crypt_fog.rb
@@ -8,43 +8,44 @@ require 'test/unit'
 require 'crypt/fog'
 
 class TC_Fog < Test::Unit::TestCase
-   def setup
-      @fog = Crypt::Fog.new('hello')
-   end
+  def setup
+    @fog = Crypt::Fog.new('hello')
+  end
 
-   def test_crypt_fog_version
-      assert_equal('1.0.2', Crypt::Fog::VERSION)
-   end
+  def test_crypt_fog_version
+    assert_equal('1.0.3', Crypt::Fog::VERSION)
+    assert_true(Crypt::Fog::VERSION.frozen?)
+  end
 
-   def test_crypt_fog_constructor_one_argument_form
-      assert_nothing_raised{ Crypt::Fog.new('string') }
-      assert_kind_of(String, Crypt::Fog.new('string'))
-   end
+  def test_crypt_fog_constructor_one_argument_form
+    assert_nothing_raised{ Crypt::Fog.new('string') }
+    assert_kind_of(String, Crypt::Fog.new('string'))
+  end
 
-   def test_crypt_fog_constructor_two_argument_form
-      assert_nothing_raised{ Crypt::Fog.new('string', 55) }
-      assert_kind_of(String, Crypt::Fog.new('string', 55))
-   end
+  def test_crypt_fog_constructor_two_argument_form
+    assert_nothing_raised{ Crypt::Fog.new('string', 55) }
+    assert_kind_of(String, Crypt::Fog.new('string', 55))
+  end
 
-   def test_class_decrypt
-      assert_respond_to(Crypt::Fog, :decrypt)
-      assert_nothing_raised{ Crypt::Fog.decrypt('string') }
-      assert_nothing_raised{ Crypt::Fog.decrypt('string', 66) }
-      assert_equal('hello', Crypt::Fog.decrypt(';8??B', 2003))
-   end
+  def test_class_decrypt
+    assert_respond_to(Crypt::Fog, :decrypt)
+    assert_nothing_raised{ Crypt::Fog.decrypt('string') }
+    assert_nothing_raised{ Crypt::Fog.decrypt('string', 66) }
+    assert_equal('hello', Crypt::Fog.decrypt(';8??B', 2003))
+  end
 
-   def test_instance_decrypt
-      assert_respond_to(@fog, :decrypt)
-      assert_nothing_raised{ @fog.decrypt }
-      assert_equal('hello', @fog.decrypt)
-   end
+  def test_instance_decrypt
+    assert_respond_to(@fog, :decrypt)
+    assert_nothing_raised{ @fog.decrypt }
+    assert_equal('hello', @fog.decrypt)
+  end
 
-   def test_types
-      assert_kind_of(Crypt::Fog, @fog)
-      assert_kind_of(String, @fog.decrypt)
-   end
+  def test_types
+    assert_kind_of(Crypt::Fog, @fog)
+    assert_kind_of(String, @fog.decrypt)
+  end
 
-   def teardown
-      @fog = nil
-   end
+  def teardown
+    @fog = nil
+  end
 end
